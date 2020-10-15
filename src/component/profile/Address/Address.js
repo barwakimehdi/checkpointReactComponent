4c4497b58a84c475d3966eae4d2b50b481598b98
Mehdi Barwaki <barwakimehdi@gmail.com>
2020-10-15 12:12:58 +0100

    first commit

---
+++ src/component/profile/Address/Address.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import "./AddressStyle.css"
+
+ class Address extends Component {
+    render() {
+        return (
+            <div>
+                <p>3 rue d'éclat HAMMAM CHATT Ben Arous</p>
+            </div>
+        )
+    }
+}
+
+export default Address
---
+++ src/component/profile/ProfilPhoto/ProfilPhoto.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './ProfilePhotoStyle.css'
+
+ class ProfilPhoto extends Component {
+    render() {
+        return (
+            <div >
+               <img src="/image/MehdiBarwaki.jpg" alt="profile" />
+            </div>
+        )
+    }
+}
+
+export default ProfilPhoto
